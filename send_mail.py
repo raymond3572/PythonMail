b9932bfb6b509e3e693febc38ce020469b53da07
Raymond <raymond3572@gmail.com>
2022-03-09 09:38:35 +0200

    Changes Details

--- send_mail.py
+++ send_mail.py
@@ -4,9 +4,9 @@ from email.message import EmailMessage
 
 subject = "Social Media Latest Post Notification"
 body = "Hi <br><br>Check out our social media pages to see our latest posts.<br><br>Please like, share, or copy and paste our content to your LinkedIn profile status update.<br>Click on the links below to visit our social media pages: <br><br>https://www.facebook.com/RahnConsolidated <br> https://www.linkedin.com/company/rahn-consolidated-itconsulting-businessanalytics-bireporting-itrecruitment-financialcrimeproducts <br>https://www.instagram.com/rahnconsolidated/ <br><br> Kind regards"
-sender_email = "rahnconsolidated@gmail.com"
+sender_email = "raymond3572@gmail.com"
 receiver_email = "raymondvdb@rahn.co.za"
-password = "Sabba2020"
+password = "Rambo1010#"
 
 message = EmailMessage()
 message["From"] = sender_email
